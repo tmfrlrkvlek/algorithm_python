a0dfe9d0820b516f0c3a5f3c277e7ae83fc15da0
wltn2814 <wltn102998@gmail.com>
2021-02-08 23:21:32 +0900

    210208 Baekjoon - challenging #1024

---
+++ Baekjoon/Challenging/1024.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void){
+
+    long long num;
+    long long len;
+    scanf("%lld %lld", &num, &len);
+    while(num % 2 == 0 && len % 2 == 0 && len <= 100){
+        len++;
+    }
+    if(len + 1> 100){
+        printf("-1");
+    }else{
+        for(long long i = -(len/2); i <= len/2; i++){
+            printf("%lld ", num/len + i);
+        } 
+    }
+}
