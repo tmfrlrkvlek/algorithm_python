848e3fe791bd7352dee0e023c061074dac32f69a
JISU LEE <wltn102998@gmail.com>
2022-01-10 23:49:50 +0900

    successful: baekjoon - python 10819, 10974, 13423, 1436, 1895, 1969, 6603 swift 13423

---
+++ Baekjoon/Successful/python/1436.py
@@ -0,0 +1,11 @@
+# 1436
+import sys
+cnt = 0
+num = 666
+n = int(sys.stdin.readline())
+while True :
+    if '666' in str(num) : cnt += 1
+    if cnt == n : 
+        print(num); exit()
+    num += 1
+
