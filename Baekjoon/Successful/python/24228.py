f7f2cbb135befa6c044f23eb063db571308edf2a
JISU LEE <wltn102998@gmail.com>
2022-01-28 21:05:10 +0900

    successful: baekjoon python 14465, 15565, 15658, 17175, 24228 swift 1051

---
+++ Baekjoon/Successful/python/24228.py
@@ -0,0 +1,22 @@
+# 24228
+import abc
+import sys
+n, r = map(int, sys.stdin.readline().strip().split())
+print(n+2*(r-1)+1)
+
+# 3
+# abc
+# 4
+# abca -> 1
+# 5
+# abcaa -> 1
+# 6
+# abcaab -> 2
+# 7
+# abcaabb -> 2
+# 8
+# abcaabba -> 3
+# 9
+# abcaabbaa -> 3
+# 10
+# abcaabbaac -> 3
